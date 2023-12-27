1a91b1e817a67639bbb7d46f982093f6bb246a06
stepanvalic <valicekstepan8@seznam.cz>
2023-12-27 17:51:01 +0100

    create czech translation

---
+++ locales/cs.lua
@@ -0,0 +1,20 @@
+local Translations = {
+    error = {
+        invalid_ext = "Thats not a valid extension, only %{fileext} extension links are allowed.",
+    },
+    info = {
+        use_printer = "Použít tiskárnu"
+
+    },
+    command = {
+        spawn_printer = "spawnout tiskárnu"
+    }
+}
+
+if GetConvar('qb_locale', 'en') == 'cs' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
