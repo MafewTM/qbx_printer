ccecc83a376d2f5684bd742854d7ef837a3415c7
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-12-27 12:36:35 -0500

    refactor(server): camelCase, ' over ", and oxify

--- server/main.lua
+++ server/main.lua
@@ -1,35 +1,33 @@
-local QBCore = exports['qbx-core']:GetCoreObject()
-
-local ValidExtensions = {
-    [".png"] = true,
-    [".gif"] = true,
-    [".jpg"] = true,
-    ["jpeg"] = true
+local ITEMS = exports.ox_inventory:Items()
+local validExtensions = {
+    ['.png'] = true,
+    ['.gif'] = true,
+    ['.jpg'] = true,
+    ['jpeg'] = true
 }
 
-local ValidExtensionsText = '.png, .gif, .jpg, .jpeg'
+local validExtensionsText = '.png, .gif, .jpg, .jpeg'
 
-QBCore.Functions.CreateUseableItem("printerdocument", function(source, item)
+exports.qbx_core:CreateUseableItem('printerdocument', function(source, item)
     TriggerClientEvent('qbx_printer:client:useDocument', source, item)
 end)
 
-QBCore.Commands.Add("spawnprinter", Lang:t('command.spawn_printer'), {}, true, function(source, _)
-	TriggerClientEvent('qbx_printer:client:spawnPrinter', source)
-end, "admin")
+lib.command('spawnprinter', {help = Lang:t('command.spawn_printer'), restricted = 'group.admin'}, function(source)
+    TriggerClientEvent('qbx_printer:client:spawnPrinter', source)
+end)
 
 RegisterNetEvent('qbx_printer:server:saveDocument', function(url)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
+    local player = exports.qbx_core:GetPlayer(src)
     local info = {}
     local extension = string.sub(url, -4)
-    local validexts = ValidExtensions
     if url ~= nil then
-        if validexts[extension] then
+        if validExtensions[extension] then
             info.url = url
-            Player.Functions.AddItem('printerdocument', 1, nil, info)
-            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['printerdocument'], "add")
+            player.Functions.AddItem('printerdocument', 1, nil, info)
+            exports.ox_inventory:AddItem(src, 'printerdocument', 1)
         else
-            TriggerClientEvent('ox_lib:notify', src, { description = Lang:t('error.invalid_ext', {fileext = ValidExtensionsText}), type = 'error' })
+            exports.qbx_core:Notify(src, Lang:t('error.invalid_ext', {fileext = validExtensionsText}), 'error')
         end
     end
 end)
