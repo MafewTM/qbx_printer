883c4ecc7107e3fcff4958e9764bacef8101d0aa
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2023-12-28 01:00:30 +0000

    allow webp images. why not?

--- server/main.lua
+++ server/main.lua
@@ -1,11 +1,12 @@
 local validExtensions = {
-    ['.png'] = true,
-    ['.gif'] = true,
-    ['.jpg'] = true,
-    ['jpeg'] = true
+    ['png'] = true,
+    ['gif'] = true,
+    ['jpg'] = true,
+    ['jpeg'] = true,
+    ['webp'] = true
 }
 
-local validExtensionsText = '.png, .gif, .jpg, .jpeg, www'
+local validExtensionsText = '.png, .gif, .jpg, .jpeg, .webp'
 
 exports.qbx_core:CreateUseableItem('printerdocument', function(source, item)
     TriggerClientEvent('qbx_printer:client:useDocument', source, item)
@@ -16,11 +17,13 @@ lib.addCommand('spawnprinter', {help = Lang:t('command.spawn_printer'), restrict
 end)
 
 RegisterNetEvent('qbx_printer:server:saveDocument', function(url)
+    print(url)
     local src = source
     local info = {}
     local extension = string.sub(url, -4)
     if url ~= nil then
         if validExtensions[extension] then
+            print(validExtensions[extension])
             info.url = url
             exports.ox_inventory:AddItem(src, 'printerdocument', 1, info)
         else
